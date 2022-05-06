746516596be3fa881015199a1c284ef54862c162
Dean Langton <dean.langton@uq.net.au>
2022-05-07 00:02:33 +1000

    plot hook

---
+++ js/hookList.js
@@ -0,0 +1,3 @@
+export const plotHookLocations = ["In a tavern", "At the town noticeboard", "In the city jail", "At a seasonal festival", "At a small way-camp", "Near a long forgotton ruin", "At the local docks", "On the road with a noble's caravan", "On a farmer's land", "At the edge of a dark forest", "At a grand feast", "On a sailing ship", "Near the abandoned church", ""];
+
+export const plotHookReasons = ["after receiving a letter from an old friend", "after hearing news of a mysterious cave", "needing to urgently see the mayor", "fleeing from the city guard", "searching for a missing person", "hunting a local monster", "in search of a missing item", "out to catch notorious criminal", ""];
